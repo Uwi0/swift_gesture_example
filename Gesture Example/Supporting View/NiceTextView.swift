2617a9bdad61412f3beb9dd523a679eebfa5afc1
uwi0 <kakapoweb@gmail.com>
2024-08-12 20:27:56 +0700

    Create Nice Text View

--- Gesture Example/Supporting View/NiceTextView.swift
+++ Gesture Example/Supporting View/NiceTextView.swift
@@ -1,8 +1,24 @@
-//
-//  NiceTextView.swift
-//  Gesture Example
-//
-//  Created by dwi prasetyo on 12/08/24.
-//
+import SwiftUI
 
-import Foundation
+struct NiceTextView: View {
+    let text: String
+    let bgColor: Color
+    let fgColor: Color
+    let radius: CGFloat
+    
+    var body: some View {
+        Text("Hello world")
+            .font(.title)
+            .fontWeight(.semibold)
+            .foregroundStyle(fgColor)
+            .padding()
+            .background(bgColor)
+            .cornerRadius(radius)
+    }
+}
+
+struct NiceTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        NiceTextView(text: "hello", bgColor: .black, fgColor: .orange, radius: 10)
+    }
+}
