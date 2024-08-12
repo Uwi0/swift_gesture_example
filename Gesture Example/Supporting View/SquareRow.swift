bfab19fd68cd6f0e9a388444f95714b165c1a3ed
uwi0 <kakapoweb@gmail.com>
2024-08-12 20:50:57 +0700

    creating squares view

---
+++ Gesture Example/Supporting View/SquareRow.swift
@@ -0,0 +1,18 @@
+//
+//  SquareRow.swift
+//  Gesture Example
+//
+//  Created by dwi prasetyo on 12/08/24.
+//
+
+import SwiftUI
+
+struct SquareRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SquareRow()
+}
---
+++ Gesture Example/Supporting View/Squares.swift
@@ -0,0 +1,18 @@
+//
+//  Squares.swift
+//  Gesture Example
+//
+//  Created by dwi prasetyo on 12/08/24.
+//
+
+import SwiftUI
+
+struct Squares: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Squares()
+}
