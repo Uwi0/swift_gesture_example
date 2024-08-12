0adddc82b5a9b534021ace1241a20340c7ee8d81
uwi0 <kakapoweb@gmail.com>
2024-08-12 19:11:54 +0700

    Initial Commit

---
+++ Gesture Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gesture Example
+//
+//  Created by dwi prasetyo on 12/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gesture Example/Gesture_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gesture_ExampleApp.swift
+//  Gesture Example
+//
+//  Created by dwi prasetyo on 12/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Gesture_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
